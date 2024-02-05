9088d2138cfe6862dbd97adf031fe2252fa7d92a
Pritam Basnet <coderpritam951@gmail.com>
2024-02-05 21:26:08 +0545

    hello

---
+++ js/app.js
@@ -0,0 +1,47 @@
+function sheryJs() {
+  Shery.mouseFollower({
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.5,
+  });
+  Shery.textAnimate("#my-name" /* Element to target.*/, {
+    //Parameters are optional.
+    style: 2,
+    y: 10,
+    delay: 0.1,
+    duration: 0.5,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+  });
+
+  Shery.makeMagnet("#my-name,nav>ul>li>a>h5" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+}
+sheryJs();
+function jsOnWorkSection() {
+  let categoryOfWork = document.querySelectorAll('.category-list>ul>li');
+  let indexOfActiveGrid = 0;
+  let grids = document.querySelectorAll(".work-container>.grid");
+  categoryOfWork[indexOfActiveGrid].classList.add('active-list');
+  grids[indexOfActiveGrid].classList.add('grid-active');
+  categoryOfWork.forEach((category,categoryIndex)=>{
+    category.addEventListener("click",()=>{
+      indexOfActiveGrid = categoryIndex;
+      upgradeClass(categoryIndex);
+    });
+  });
+  function upgradeClass(categoryIndex) {
+    categoryOfWork.forEach((category)=>{
+      category.classList.remove('active-list')
+    })
+    grids.forEach((grid)=>{
+      grid.classList.remove('grid-active')
+    })
+    categoryOfWork[indexOfActiveGrid].classList.add('active-list');
+    grids[indexOfActiveGrid].classList.add('grid-active');
+  }
+}
+jsOnWorkSection();
