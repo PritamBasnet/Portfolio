adedb84397d2443728137fc39a9a9aefb0d5922e
Pritam Basnet <coderpritam951@gmail.com>
2024-02-06 12:22:49 +0545

    hiiii

--- js/app.js
+++ js/app.js
@@ -1,76 +1,77 @@
 // gsap.registerPlugin(ScrollTrigger);
 // Fix mobile version
 ScrollTrigger.observe({
-  trigger: 'body',
+  trigger: "body",
   type: "touch,pointer", // comma-delimited list of what to listen for ("wheel,touch,scroll,pointer")
-  onUp: () => { ScrollTrigger.update(); },
+  onUp: () => {
+    ScrollTrigger.update();
+  },
 });
-function sheryJs() {
-  // Shery.mouseFollower({
-  //   //Parameters are optional.
-  //   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  //   duration: 0.5,
-  // }); 
-  Shery.textAnimate("#my-name" /* Element to target.*/, {
-    //Parameters are optional.
-    style: 2,
-    y: 10,
-    delay: 0.1,
-    duration: 0.5,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    multiplier: 0.1,
-  });
+// function sheryJs() {
+//   // Shery.mouseFollower({
+//   //   //Parameters are optional.
+//   //   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   //   duration: 0.5,
+//   // });
+//   Shery.textAnimate("#my-name" /* Element to target.*/, {
+//     //Parameters are optional.
+//     style: 2,
+//     y: 10,
+//     delay: 0.1,
+//     duration: 0.5,
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     multiplier: 0.1,
+//   });
 
-  Shery.makeMagnet("#my-name,nav>ul>li>a>h5" /* Element to target.*/, {
-    //Parameters are optional.
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
-}
-sheryJs();
+//   Shery.makeMagnet("#my-name,nav>ul>li>a>h5" /* Element to target.*/, {
+//     //Parameters are optional.
+//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//     duration: 1,
+//   });
+// }
+// sheryJs();
 function jsOnWorkSection() {
-  let categoryOfWork = document.querySelectorAll('.category-list>ul>li');
+  let categoryOfWork = document.querySelectorAll(".category-list>ul>li");
   let indexOfActiveGrid = 0;
   let grids = document.querySelectorAll(".work-container>.grid");
-  categoryOfWork[indexOfActiveGrid].classList.add('active-list');
-  grids[indexOfActiveGrid].classList.add('grid-active');
-  categoryOfWork.forEach((category,categoryIndex)=>{
-    category.addEventListener("click",()=>{
+  categoryOfWork[indexOfActiveGrid].classList.add("active-list");
+  grids[indexOfActiveGrid].classList.add("grid-active");
+  categoryOfWork.forEach((category, categoryIndex) => {
+    category.addEventListener("click", () => {
       indexOfActiveGrid = categoryIndex;
       upgradeClass(categoryIndex);
     });
   });
   function upgradeClass(categoryIndex) {
-    categoryOfWork.forEach((category)=>{
-      category.classList.remove('active-list')
-    })
-    grids.forEach((grid)=>{
-      grid.classList.remove('grid-active')
-    })
-    categoryOfWork[indexOfActiveGrid].classList.add('active-list');
-    grids[indexOfActiveGrid].classList.add('grid-active');
+    categoryOfWork.forEach((category) => {
+      category.classList.remove("active-list");
+    });
+    grids.forEach((grid) => {
+      grid.classList.remove("grid-active");
+    });
+    categoryOfWork[indexOfActiveGrid].classList.add("active-list");
+    grids[indexOfActiveGrid].classList.add("grid-active");
   }
 }
 jsOnWorkSection();
 
 function gsapOnSkill() {
-  let getWidth = 0;
-  document.querySelectorAll('.skill div img').forEach(()=>{
-    getWidth = getWidth +150 +30;
-  })
-
+  let reactOfDiv = document.querySelector('.skill>div').getBoundingClientRect();
+  function getScrollAmount() {
+    return -(reactOfDiv.width-window.innerWidth+90);
+  }
   gsap.to('.skill div',{
-    transform:`translateX(-${getWidth}px)`,
-    ease: "power1.inOut", 
-    // duration:4,
+    x:getScrollAmount,
+    ease:"none",
     scrollTrigger:{
-      trigger:"main",
-      scroller:"body",
-      start:"top 0%",
-      scrub:1,
-      end:"top -100%",
+      trigger:'.skill',
+      start:'top 20%',
+      end:`+=${getScrollAmount()*-1}`,
       pin:true,
+      scrub:1,
+      invalidateOnReferesh:true,
+      // markers:true,
     }
-  });
+  })
 }
 gsapOnSkill();
