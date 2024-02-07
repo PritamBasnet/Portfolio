e75da326cb42161aee1cb7dde9c018b796e60cee
Pritam Basnet <coderpritam951@gmail.com>
2024-02-07 11:47:53 +0545

    update on scrollTrigger

--- js/app.js
+++ js/app.js
@@ -7,29 +7,29 @@ ScrollTrigger.observe({
     ScrollTrigger.update();
   },
 });
-// function sheryJs() {
-//   // Shery.mouseFollower({
-//   //   //Parameters are optional.
-//   //   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-//   //   duration: 0.5,
-//   // });
-//   Shery.textAnimate("#my-name" /* Element to target.*/, {
-//     //Parameters are optional.
-//     style: 2,
-//     y: 10,
-//     delay: 0.1,
-//     duration: 0.5,
-//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-//     multiplier: 0.1,
-//   });
+function sheryJs() {
+  Shery.mouseFollower({
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.5,
+  });
+  Shery.textAnimate("#my-name" /* Element to target.*/, {
+    //Parameters are optional.
+    style: 2,
+    y: 10,
+    delay: 0.1,
+    duration: 0.5,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+  });
 
-//   Shery.makeMagnet("#my-name,nav>ul>li>a>h5" /* Element to target.*/, {
-//     //Parameters are optional.
-//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-//     duration: 1,
-//   });
-// }
-// sheryJs();
+  Shery.makeMagnet("#my-name,nav>ul>li>a>h5" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+}
+sheryJs();
 function jsOnWorkSection() {
   let categoryOfWork = document.querySelectorAll(".category-list>ul>li");
   let indexOfActiveGrid = 0;
@@ -55,23 +55,31 @@ function jsOnWorkSection() {
 }
 jsOnWorkSection();
 
-function gsapOnSkill() {
-  let reactOfDiv = document.querySelector('.skill>div').getBoundingClientRect();
+// Function to animate the skill section using GSAP ScrollTrigger
+function animateSkillSection() {
+  // Get the width of the inner content of the skill section
+  let skillInnerWidth = document
+    .querySelector(".skill>div")
+    .getBoundingClientRect().width;
+
+  // Calculate the scroll amount based on the inner content width and the window width
   function getScrollAmount() {
-    return -(reactOfDiv.width-window.innerWidth+90);
+    return -(skillInnerWidth - window.innerWidth + 90);
   }
-  gsap.to('.skill div',{
-    x:getScrollAmount,
-    ease:"none",
-    scrollTrigger:{
-      trigger:'.skill',
-      start:'top 20%',
-      end:`+=${getScrollAmount()*-1}`,
-      pin:true,
-      scrub:1,
-      invalidateOnReferesh:true,
-      // markers:true,
-    }
-  })
+
+  // Use GSAP to animate the skill section based on scroll trigger
+  gsap.to(".skill div", {
+    x: getScrollAmount,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".skill",
+      start: "top 20%",
+      end: `+=${getScrollAmount() * -1}`,
+      pin: true,
+      scrub: 1,
+    },
+  });
 }
-gsapOnSkill();
+
+// Call the function to initiate the animation
+animateSkillSection();
